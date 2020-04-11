2c03c2a8bf861df6927c7fcc3223d73c3a8c2133
Mirosław Dubaj <miroslaw.dubaj@gmail.com>
2020-04-11 22:45:07 +0200

    Sample template

---
+++ template-sample.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Template Name: Sample Page
+ *
+ */
+
+ ?>
+
+ <?php get_header(); ?>
+ <?php the_title('&#9733; ', ' &#9733;'); ?>
+<?php the_post_thumbnail('thumbnail') ?>
+ <?php get_footer(); ?>
