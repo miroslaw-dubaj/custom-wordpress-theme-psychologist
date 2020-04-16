3015b87dc0f46218a576b4e8c327e9b433900cf5
Mirosław Dubaj <miroslaw.dubaj@gmail.com>
2020-04-16 22:52:12 +0200

    Tiny changes

--- footer.php
+++ footer.php
@@ -15,17 +15,12 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'marzenakwasik' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'marzenakwasik' ), 'WordPress' );
+				printf( esc_html__( '&#169; 2020 Centrum Diagnozy i Terapii – Marzena Kwasik', 'marzenakwasik' ), 'WordPress' );
 				?>
-			</a>
 			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'marzenakwasik' ), 'marzenakwasik', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+				Strona stworzona przez <a href="https://dubaj.dev/">dubaj.dev</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<section class="hero">
+    <?php marzenakwasik_post_thumbnail(); ?>
+</section>
+
+<?php get_footer(); ?>
